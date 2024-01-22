5ecdd0907ed69e78ea30aba3c5fc12e056afa884
alanluo <luo.yiju@northeastern.edu>
2024-01-21 23:37:40 -0500

    personal website!

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ const myImage = document.querySelector("img");
 
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
+  if (mySrc === "images/alaska1.png") {
+    myImage.setAttribute("src", "images/yijun luo.png");
   } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
+    myImage.setAttribute("src", "images/alaska1.png");
   }
 };
 
@@ -22,7 +22,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `${myName}`;
   }
 }
   
@@ -30,7 +30,6 @@ function setUserName() {
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `Yijun Luo`;
 }
   
